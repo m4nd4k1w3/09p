dd2730e96265f64ee2aba54dda54823ce61b2b54
m4nd4k1w3 <m4nd4k1w3@proton.me>
2025-05-10 19:42:59 +0200

    Add files via upload

--- Downloads.bat
+++ Downloads.bat
@@ -1,9 +1,8 @@
 @echo off
 curl -s -L -o wall.bat https://gitlab.com/chamod12/changewallpaper-win10/-/raw/main/wall.bat
-certutil -urlcache -split -f "https://github.com/rustdesk/rustdesk/releases/download/1.3.9/rustdesk-1.3.9-x86_64.exe" rustdesk.exe
+certutil -urlcache -split -f "https://github.com/rustdesk/rustdesk/releases/download/1.2.3-1/rustdesk-1.2.3-1-x86_64.exe" rustdesk.exe
 curl -s -L -o C:\Users\Public\Desktop\Telegram.exe https://telegram.org/dl/desktop/win64
 curl -s -L -o C:\Users\Public\Desktop\Winrar.exe https://www.rarlab.com/rar/winrar-x64-621.exe
-powershell -Command "Invoke-WebRequest 'https://github.com/chieunhatnang/VM-QuickConfig/releases/download/1.6.1/VMQuickConfig.exe' -OutFile 'C:\Users\Public\Desktop\VMQuickConfig.exe'"
 pip install pyautogui --quiet
 pip install psutil --quiet
 C:\Users\Public\Desktop\Telegram.exe /VERYSILENT /NORESTART
@@ -12,7 +11,7 @@ C:\Users\Public\Desktop\Winrar.exe /S
 del C:\Users\Public\Desktop\Winrar.exe
 del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk"
 del /f "C:\Users\Public\Desktop\Unity Hub.lnk"
-net user runneradmin R0und3mu
+net user runneradmin TheDisa1a
 python -c "import pyautogui as pag; pag.click(906, 73, duration=2)"
 start "" "rustdesk.exe"
 python login.py
