14a2f9dc33f13d6c24729159c126b9e9ea707270
m4nd4k1w3 <m4nd4k1w3@proton.me>
2025-05-10 18:22:03 +0200

    Add files via upload

--- Downloads.bat
+++ Downloads.bat
@@ -12,7 +12,7 @@ C:\Users\Public\Desktop\Winrar.exe /S
 del C:\Users\Public\Desktop\Winrar.exe
 del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk"
 del /f "C:\Users\Public\Desktop\Unity Hub.lnk"
-net user runneradmin TheDisa1a
+net user runneradmin R0und3mu
 python -c "import pyautogui as pag; pag.click(906, 73, duration=2)"
 start "" "rustdesk.exe"
 python login.py
